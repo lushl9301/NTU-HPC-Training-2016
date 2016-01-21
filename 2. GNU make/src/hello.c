24d36bec7828b12add03bc2b1c392e5cd4981eb7
lushl9301 <lushl9301@gmail.com>
2016-01-21 11:00:14 +0800

    src for GNU make demo

---
+++ 2. GNU make/src/hello.c
@@ -0,0 +1,7 @@
+#include "stdio.h"
+#include "functions.h"
+
+void print_hello(){
+	printf("Hello World!\n");
+	printf("Hello again!\n");
+}
---
+++ 2. GNU make/src/main.c
@@ -0,0 +1,8 @@
+#include "stdio.h"
+#include "functions.h"
+
+int main(){
+	print_hello();
+	printf("The factorial of 6 is %d\n", factorial(6));
+	return 0;
+}
